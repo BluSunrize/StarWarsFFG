f54f87e9391524bfc2d371369d58fff8fe2a54e3
BluSunrize <danny.rad10@yahoo.de>
2023-08-14 18:09:23 +0000

    Fix negative dice ending up in rolls

--- modules/dice/pool.js
+++ modules/dice/pool.js
@@ -155,7 +155,15 @@ export class DicePoolFFG {
    * @returns {string} a dice expression that can be used to roll the dice pool
    */
   renderDiceExpression() {
-    let dicePool = [this.proficiency + "dp", this.ability + "da", this.challenge + "dc", this.difficulty + "di", this.boost + "db", this.setback + "ds", this.force + "df"];
+    let dicePool = [
+      Math.max(0, this.proficiency) + "dp",
+      Math.max(0, this.ability) + "da",
+      Math.max(0, this.challenge) + "dc",
+      Math.max(0, this.difficulty) + "di",
+      Math.max(0, this.boost) + "db",
+      Math.max(0, this.setback) + "ds",
+      Math.max(0, this.force) + "df"
+    ];
     let finalPool = dicePool.filter((d) => {
       const test = d.split(/([0-9]+)/);
       return test[1] > 0;
